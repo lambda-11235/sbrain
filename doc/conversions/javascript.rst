e251a52d35b8dd31bbec31b01a6eba655707581a
Taran Lynn <taranlynn0@gmail.com>
2016-05-26 15:10:31 -0700

    Moved read and write operations to seperate functions for JS.
    Added explicit semicolons after all JS statements.

--- doc/conversions/javascript.rst
+++ doc/conversions/javascript.rst
@@ -5,30 +5,37 @@ Javascript
 start
 -----
 
-| arr = []
+| arr = [];
 |
-| for(var i = 0; i < 30000; i++) {arr[i] = 0}
+| for(var i = 0; i < 30000; i++) {arr[i] = 0;}
 |
-| idx = 0
-| input = ""
-
+| idx = 0;
+| input = "";
+|
+| function read() {
+|     while(input.length == 0) {input = prompt("Enter text")}
+|     arr[idx] = input.charCodeAt(0);
+|     input = input.substring(1);
+| }
+|
+| function write() {
+|     document.write(String.fromCharCode(arr[idx]));
+| }
 
 Conversions
 -----------
 
 | ModPointer(#)
-|     idx += #
+|     idx += #;
 |
 | ModValue(#)
-|     arr[idx] += #
+|     arr[idx] += #;
 |
 | Read(#) - Repeat # times.
-|     while(input.length == 0) {input = prompt("Enter text")}
-|     arr[idx] = input.charAt(0)
-|     input = input.substring(1)
+|     read();
 |
 | Write(#) - Repeat # times.
-|     document.write(String.fromCharCode(arr[idx]))
+|     write();
 |
 | Loop(#, ...)
 |     while(arr[idx] != 0) {
--- src/main/scala/brain/compile/javascript.scala
+++ src/main/scala/brain/compile/javascript.scala
@@ -12,29 +12,35 @@ object JavaScript {
    * @param numCells The number of cells supported at runtime.
    */
   def irToJS(ir: List[IR], numCells: Int): String = {
-    val init = ("arr = []\n\n"
+    val init = ("arr = [];\n"
                 + ("for(var i = 0; i < " + numCells.toString
-                   + "; i++) {arr[i] = 0}\n\n")
-                + "idx = 0\n"
-                + "input = \"\"\n\n")
+                   + "; i++) {arr[i] = 0;}\n")
+                + "idx = 0;\n"
+                + "input = \"\";\n")
+
+    val read = ("function read() {"
+                + "while(input.length == 0) {input = prompt(\"Enter text\")}"
+                + "arr[idx] = input.charCodeAt(0);"
+                + "input = input.substring(1);"
+                + "}\n")
+
+    val write = ("function write() {"
+                 + "document.write(String.fromCharCode(arr[idx]));"
+                 + "}\n")
 
     val main = ir.map(convertIR).fold("")(_+_)
 
-    init + main
+    init + read + write + main
   }
 
   def convertIR(ir: IR): String = ir match {
-    case ModPointer(amount) => "idx += " + amount.toString + "\n"
+    case ModPointer(amount) => "idx += " + amount.toString + ";\n"
 
-    case ModValue(amount) => "arr[idx] += " + amount.toString + "\n"
+    case ModValue(amount) => "arr[idx] += " + amount.toString + ";\n"
 
-    case Read(times) => List.fill(times)(
-      "while(input.length == 0) {input = prompt(\"Enter text\")}\n"
-      + "arr[idx] = input.charCodeAt(0)\n"
-      + "input = input.substring(1)\n").fold("")(_+_)
+    case Read(times) => List.fill(times)("read();\n").fold("")(_+_)
 
-    case Write(times) => List.fill(times)("document.write(String.fromCharCode"
-                                          + "(arr[idx]))\n").fold("")(_+_)
+    case Write(times) => List.fill(times)("write();\n").fold("")(_+_)
 
     case Loop(id, body) => ("while(arr[idx] != 0) {\n"
                             + body.map(convertIR).fold("")(_+_)
